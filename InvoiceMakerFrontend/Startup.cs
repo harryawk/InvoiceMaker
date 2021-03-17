799dbff92f45e790b3282b8ecc48acd5d085d457
harryawk <harry.kambey@gmail.com>
2021-03-18 02:39:33 +0700

    [frontend] change language base URL

--- InvoiceMakerFrontend/Startup.cs
+++ InvoiceMakerFrontend/Startup.cs
@@ -23,7 +23,7 @@ namespace InvoiceMakerFrontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient("languageClient", c => c.BaseAddress = new System.Uri("https://localhost:44388/weatherforecast"));
+            services.AddHttpClient("languageClient", c => c.BaseAddress = new System.Uri("https://localhost:44388/api/language"));
             services.AddRazorPages();
         }
 
