6a1835b667e4cf92167cdd20d0e846d5fd704a13
harryawk <harry.kambey@gmail.com>
2021-03-18 10:26:43 +0700

    add company model and API

---
+++ InvoiceMaker/Controllers/CompanyController.cs
@@ -0,0 +1,83 @@
+﻿using InvoiceMaker.Data;
+using InvoiceMaker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceMaker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        public readonly ApplicationDbContext _db;
+
+        public CompanyController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _db.Companies.ToListAsync());
+        }
+
+        [HttpGet("detail/{id}")]
+        public async Task<IActionResult> GetDetail(int id)
+        {
+            return Ok(await _db.Companies.FindAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Companies.AddAsync(company);
+                await _db.SaveChangesAsync();
+                return Accepted();
+            }
+
+            return BadRequest(new { Error = "Data tidak valid." });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Company company)
+        {
+            var comp = await _db.Companies.FindAsync(id);
+            if (comp == null)
+            {
+                return BadRequest(new { Error = "Data tidak ditemukan" });
+            }
+
+            company.ID = id;
+            if (ModelState.IsValid)
+            {
+                _db.Companies.Update(company);
+                await _db.SaveChangesAsync();
+                return Ok(company);
+            }
+            return Ok(id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deletedData = await _db.Companies.FindAsync(id);
+            if (deletedData == null)
+            {
+                return BadRequest(new { Error = "Data tidak ditemukan." });
+            }
+
+            _db.Companies.Remove(deletedData);
+            await _db.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ InvoiceMaker/Models/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceMaker.Models
+{
+    public class Company
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Logo { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+    }
+}
