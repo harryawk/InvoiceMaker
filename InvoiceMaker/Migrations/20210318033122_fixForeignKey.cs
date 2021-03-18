dd47c7c9c6884337fdbbfa274b43023dddac69c9
harryawk <harry.kambey@gmail.com>
2021-03-18 10:43:23 +0700

    fix foreign key in design

---
+++ InvoiceMaker/Migrations/20210318033122_fixForeignKey.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceMaker.Migrations
+{
+    public partial class fixForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_PurchaseOrderID",
+                table: "Invoices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_PurchaseOrderID",
+                table: "Invoices",
+                column: "PurchaseOrderID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_PurchaseOrderID",
+                table: "Invoices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_PurchaseOrderID",
+                table: "Invoices",
+                column: "PurchaseOrderID",
+                unique: true);
+        }
+    }
+}
