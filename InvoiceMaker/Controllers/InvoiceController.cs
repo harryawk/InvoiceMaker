979f3e236d168f1327902e9cae4b23b3c90e7598
harryawk <harry.kambey@gmail.com>
2021-03-18 10:43:10 +0700

    add invoice model and API handlers

---
+++ InvoiceMaker/Controllers/InvoiceController.cs
@@ -0,0 +1,88 @@
+﻿using InvoiceMaker.Data;
+using InvoiceMaker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceMaker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        public readonly ApplicationDbContext _db;
+
+        public InvoiceController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _db.Invoices
+                .Include(i => i.Language)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Company)
+                .Include(i => i.Currency)
+                .ToListAsync());
+        }
+
+        [HttpGet("detail/{id}")]
+        public async Task<IActionResult> GetDetail(int id)
+        {
+            return Ok(await _db.Invoices.FindAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Invoices.AddAsync(invoice);
+                await _db.SaveChangesAsync();
+                return Accepted();
+            }
+
+            return BadRequest(new { Error = "Data tidak valid." });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Invoice invoice)
+        {
+            var inv = await _db.Invoices.FindAsync(id);
+            if (inv == null)
+            {
+                return BadRequest(new { Error = "Data tidak ditemukan" });
+            }
+
+            invoice.ID = id;
+            if (ModelState.IsValid)
+            {
+                _db.Invoices.Update(invoice);
+                await _db.SaveChangesAsync();
+                return Ok(invoice);
+            }
+            return Ok(id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deletedData = await _db.Invoices.FindAsync(id);
+            if (deletedData == null)
+            {
+                return BadRequest(new { Error = "Data tidak ditemukan." });
+            }
+
+            _db.Invoices.Remove(deletedData);
+            await _db.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ InvoiceMaker/Models/Invoice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace InvoiceMaker.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string From { get; set; }
+
+        [Required]
+        [IgnoreDataMember]
+        public int To { get; set; }
+        [ForeignKey("To")]
+        public virtual Company Company { get; set; }
+
+        public string DiscountName { get; set; }
+        public double DiscountAmount { get; set; }
+
+        [Required]
+        public DateTime InvoiceDate { get; set; }
+        [Required]
+        public DateTime InvoiceDue { get; set; }
+
+        [Required]
+        [IgnoreDataMember]
+        public int PurchaseOrderID { get; set; }
+        [ForeignKey("PurchaseOrderID")]
+        public virtual PurchaseOrder PurchaseOrder { get; set; }
+
+        [Required]
+        [IgnoreDataMember]
+        public int LanguageID { get; set; }
+        [ForeignKey("LanguageID")]
+        public virtual Language Language { get; set; }
+        
+        [Required]
+        [IgnoreDataMember]
+        public int CurrencyID { get; set; }
+        [ForeignKey("CurrencyID")]
+        public virtual Currency Currency { get; set; }
+
+
+    }
+}
