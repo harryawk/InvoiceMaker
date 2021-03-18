8cac2fc6d8c387baef809200a33545a492536640
harryawk <harry.kambey@gmail.com>
2021-03-18 10:26:06 +0700

    fix reference loop in app data

--- InvoiceMaker/Startup.cs
+++ InvoiceMaker/Startup.cs
@@ -30,7 +30,9 @@ namespace InvoiceMaker
             services.AddDbContext<ApplicationDbContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbContextConnection"))
             );
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => 
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
